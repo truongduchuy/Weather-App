6ac9b958b0b95a293029ef2e09a7531b4c59b48a
Huy Truong <huy.truong@mevry.com>
2019-11-09 23:08:01 +0700

    asynchronous timeout

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('Starting up')
+
+setTimeout(()=>{
+    console.log('Two sencond!')
+}, 2000)
+
+setTimeout(()=>{
+    console.log('Zero sencond!')
+}, 0)
+
+console.log('Finishing up!')
+
+//out put
+
+// Starting up
+// Finishing up!
+// Zero sencond!
+// Two sencond!
+
