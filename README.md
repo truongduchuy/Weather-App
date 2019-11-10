97b0117aa09c6fd46cb4555fe8276db212b4a918
Huy Truong <huy.truong@mevry.com>
2019-11-10 15:44:30 +0700

    Revert "forest request"
    This reverts commit 46433b0a29e55cc504065ee957994d008cb57e0f.

--- README.md
+++
@@ -1,4 +0,0 @@
-forest request
-https://api.darksky.net/forecast/[secret-key]/[latitude],[longitude]
-secret-key get into https://darksky.net/
-
--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const request = require('request')
+console.log('Starting up')
 
-const url = 'https://api.darksky.net/forecast/0c4be23f44cef1fafa8974bb9ca2334a/16.470410,107.596537?units=si'
+setTimeout(()=>{
+    console.log('Two sencond!')
+}, 2000)
 
-request({url, json: true}, (error, response) => {
-    console.log(`It is currently ${response.body.currently.temperature} degrees out.There is a ${response.body.currently.precipProbability*100}% chance of rain`)
-})
+setTimeout(()=>{
+    console.log('Zero sencond!')
+}, 0)
+
+console.log('Finishing up!')
+
+//out put
+
+// Starting up
+// Finishing up!
+// Zero sencond!
+// Two sencond!
+
