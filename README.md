46433b0a29e55cc504065ee957994d008cb57e0f
Huy Truong <huy.truong@mevry.com>
2019-11-10 15:43:31 +0700

    forest request

---
+++ README.md
@@ -0,0 +1,4 @@
+forest request
+https://api.darksky.net/forecast/[secret-key]/[latitude],[longitude]
+secret-key get into https://darksky.net/
+
--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-console.log('Starting up')
+const request = require('request')
 
-setTimeout(()=>{
-    console.log('Two sencond!')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/0c4be23f44cef1fafa8974bb9ca2334a/16.470410,107.596537?units=si'
 
-setTimeout(()=>{
-    console.log('Zero sencond!')
-}, 0)
-
-console.log('Finishing up!')
-
-//out put
-
-// Starting up
-// Finishing up!
-// Zero sencond!
-// Two sencond!
-
+request({url, json: true}, (error, response) => {
+    console.log(`It is currently ${response.body.currently.temperature} degrees out.There is a ${response.body.currently.precipProbability*100}% chance of rain`)
+})
